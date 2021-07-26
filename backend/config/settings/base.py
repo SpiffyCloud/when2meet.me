ff29653263db2b75b3f16da1dd53e58bae16df05
decaby7e <27081624+decaby7e@users.noreply.github.com>
2021-07-26 18:13:27 -0400

    configuration refactor

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -1,25 +1,12 @@
 """
 Django Base Settings
-
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent  # [Added one level]
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'NO-A-GOOD-KEY' # [See custom settings]
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True # [See custom settings]
-
-# ALLOWED_HOSTS = [] # [See custom settings]
-
 
 # Application definition
 
@@ -63,18 +50,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-# } # [See custom settings]
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -93,29 +68,58 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-# STATIC_URL = '/static/' # [See custom settings]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# 2021.07.16-DEA
+#
+#  MIGRATED FROM LOCAL+PROD (Common elemets) (2021.07.26)
+#
+
+# Basic Settings
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config("SECRET_KEY")
+
+ALLOWED_HOSTS = ["*"]
+
+# Installed Apps
+
+INSTALLED_APPS += [
+    "core",
+    "api",
+]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = "/static/"
+
+# Google ReCaptcha
+
+RECAPTCHA_SECRET_KEY = config("RECAPTCHA_SECRET_KEY")
+RECAPTCHA_SITE_KEY = config("RECAPTCHA_SITE_KEY")
+
+# Login Settings
+
+LOGIN_URL = "core:login"
+LOGOUT_REDIRECT_URL = "core:logout"
+
+# Rest Framework
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ]
+}
--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -1,79 +1,29 @@
+"""
+Django Local Settings
+Quick-start development settings - unsuitable for production
+See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+"""
+
 from .base import *
 
-import os
-import json
-
-from django.core.exceptions import ImproperlyConfigured
-
-# Secure Secret Key Logic
-
-# JSON-based secrets module
-with open(os.path.join(BASE_DIR, "local-secrets.json")) as f:
-    secrets = json.loads(f.read())
-
-
-def get_secret(setting, secrets=secrets):
-    """Get the secret variable or return explicit exception."""
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {0} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-
-
-# Basic Settings
-
-SECRET_KEY = get_secret("SECRET_KEY")
-
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
-
-# Installed Apps
-
-INSTALLED_APPS += [
-    "core",
-    "api",
-]
 
 # Databases
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("SQL_DATABASE") or "djangotemplate",
-        "USER": os.environ.get("SQL_USER") or get_secret("SQL_USER"),
-        "PASSWORD": os.environ.get("SQL_USER") or get_secret("SQL_PASSWORD"),
-        "HOST": os.environ.get("SQL_HOST") or "postgres",
-        "PORT": os.environ.get("SQL_PORT") or "5432",
+        "NAME": config("SQL_DATABASE", default="djangotemplate"),
+        "USER": config("SQL_USER"),
+        "PASSWORD": config("SQL_USER"),
+        "HOST": config("SQL_HOST", default="postgres") or "postgres",
+        "PORT": config("SQL_PORT", default="5432"),
     }
 }
-
-# Static Files
-
-STATIC_URL = "/static/"
 
 # Email Backend
 
 EMAIL_HOST = "mailhog"
 EMAIL_PORT = 1025
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# Login Settings
-
-LOGIN_URL = "core:login"
-LOGOUT_REDIRECT_URL = "website:home_page"
-
-# Google ReCaptcha
-
-RECAPTCHA_SECRET_KEY = get_secret("RECAPTCHA_SECRET_KEY")
-RECAPTCHA_SITE_KEY = get_secret("RECAPTCHA_SITE_KEY")
-
-# Rest Framework
-REST_FRAMEWORK = {
-    "DEFAULT_RENDERER_CLASSES": [
-        "rest_framework.renderers.JSONRenderer",
-        "rest_framework.renderers.BrowsableAPIRenderer",
-    ]
-}
-# 2021.07.16-DEA
