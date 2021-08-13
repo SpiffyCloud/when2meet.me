4300205f6ef87f2593858cfe25e4857bed8169d3
Jack Driscoll <jdriscoll98@ufl.edu>
2021-08-12 23:52:15 -0400

    mock data

--- frontend/src/components/GroupAvailability.vue
+++ frontend/src/components/GroupAvailability.vue
@@ -1,13 +1,10 @@
 <template>
-    <div id="group-availability">
-        <h3>Group Availability</h3>
-        <ScrollPanel  style="width: 100%; height: 600px">
-            <VueApexCharts
-                height="3000px"
-                :options="chartOptions"
-                :series="chartData" />
-          </ScrollPanel>
-    </div>
+  <div id="group-availability">
+    <h3>Group Availability</h3>
+    <ScrollPanel style="width: 100%; height: 600px">
+      <VueApexCharts height="3000px" :options="chartOptions" :series="chartData" />
+    </ScrollPanel>
+  </div>
 </template>
 
 <script lang="ts">
@@ -17,89 +14,84 @@ import VueApexCharts from "vue3-apexcharts";
 import ScrollPanel from "primevue/scrollpanel";
 
 export default {
-    name: "Group",
-    components: {
-        VueApexCharts,
-        ScrollPanel,
+  name: "Group",
+  components: {
+    VueApexCharts,
+    ScrollPanel,
+  },
+  props: {
+    meeting: {
+      type: Object,
+      required: true,
     },
-    props: {
-        meeting: {
-            type: Object,
-            required: true,
-        },
-    },
-    setup(props: any) {
-        const chartData = ref([] as any);
+  },
+  setup(props: any) {
+    const chartData = [] as any;
 
-        const { meeting } = toRefs(props);
-        
+    const { meeting } = toRefs(props);
 
-        const initChartData = () => {
-            const startDate = new Date(0);
-            startDate.setHours(0, 0, 0, 0);
-            // start date + 5 days
-            const endDate = new Date(startDate.getTime() + (6 * 24 * 60 * 60 * 1000));
-            // const endDate = new Date(meeting.by_end_date);
-            endDate.setHours(0);
+    const initChartData = () => {
+      const startDate = new Date();
+      startDate.setHours(0, 0, 0, 0);
+      // get number of 15 minute intervals
+      const start15MinBlock = startDate.getTime() / (1000 * 60 * 15);
 
-            const startEpoch = Math.floor(
-                startDate.getTime() / (1000 * 60 * 15)
-            );
-            const endEpoch = Math.floor(endDate.getTime() / (1000 * 60 * 15));
-            const groupAvailability = [];
-            for (let i = startEpoch; i < endEpoch; i++) {
-                groupAvailability.push(0);
-            }
+      // startDate + 5 days
+      const endDate = new Date(startDate.getTime() + 1000 * 60 * 60 * 24 * 5);
+      const end15MinBlock = endDate.getTime() / (1000 * 60 * 15);
+      const groupAvailability = [];
+      for (let i = start15MinBlock; i < end15MinBlock; i++) {
+        groupAvailability.push(0);
+      }
+      const availabilites = meeting.value.availability;
+      for (let i = 0; i < availabilites?.length; i++) {
+        const availability = availabilites[i];
+        for (let j = 0; j < availability.slots.length; j++) {
+          const slot = availability.slots[j];
+          if (slot >= start15MinBlock && slot < end15MinBlock) {
+            groupAvailability[slot - start15MinBlock] += 1;
+          }
+        }
+      }
+      // createa series of data points from the group availability
+      for (let y = 0; y < 96; y++) {
+        const rawData = [];
+        for (let x = 0; x < 5; x++) {
+          const data = groupAvailability[y + x * 96];
+          const date = new Date((y + x * 96) * 15 * 60 * 1000);
+          rawData.push({
+            x: `${date.getMonth() + 1}/${date.getDate()}`,
+            y: data,
+          });
+        }
+        const day = new Date(y * 15 * 60 * 1000);
+        chartData.push({
+          name: `${day.getHours()}:${day.getMinutes()}`,
+          data: rawData,
+        });
+      }
+    };
 
-            const availabilites = meeting.value.availability;
-            for (let i = 0; i < availabilites?.length; i++) {
-                const availability = availabilites[i];
-                for (let j = 0; j < availability.slots.length; j++) {
-                    const slot = availability.slots[j];
-                    if (slot >= startEpoch && slot < endEpoch) {
-                        groupAvailability[slot - startEpoch] += 1;
-                    }
-                }
-            }
-            // createa series of data points from the group availability
-            for (let y = 0; y < 96; y++) {
-                const rawData = [];
-                for (let x = 0; x < 5; x++) {
-                    const data = groupAvailability[y + x * 96];
-                    const date = new Date((y + x * 96) * 15 * 60 * 1000);
-                    rawData.push({
-                        x: `${date.getMonth() + 1}/${date.getDate()}`,
-                        y: 1,
-                    });
-                }
-                const day = new Date(y * 15 * 60 * 1000);
-                chartData.value.push({
-                    name: `${day.getHours()}:${day.getMinutes()}`,
-                    data: rawData,
-                });
-            }
-        };
+    const chartOptions = {
+      chart: {
+        type: "heatmap",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      title: {
+        text: "HeatMap Chart (Single color)",
+      },
+      colors: ["#32a852"],
+    };
 
-        const chartOptions = {
-            chart: {
-                type: "heatmap",
-            },
-            dataLabels: {
-                enabled: false,
-            },
-            title: {
-                text: "HeatMap Chart (Single color)",
-            },
-            colors: ["#32a852"],
-        };
+    onMounted(initChartData);
 
-        onMounted(initChartData);
-
-        return {
-            chartData,
-            chartOptions,
-            initChartData,
-        };
-    },
+    return {
+      chartData,
+      chartOptions,
+      initChartData,
+    };
+  },
 };
 </script>
--- frontend/src/composables/useGetMeeting.ts
+++ frontend/src/composables/useGetMeeting.ts
@@ -8,13 +8,45 @@ export default function useGetMeeting() {
     const route = useRoute();
 
     const meeting = ref({} as meeting);
+
+    const generateData =  (numPoints: number, options: { min: number; max: number }) => {
+      const data = [];
+      for (let i = 0; i < numPoints; i++) {
+        data.push(
+          Math.floor(Math.random() * (options.max - options.min) + options.min)
+        );
+      }
+      return data;
+    }
     const getMeeting = async () => {
       meeting.value = await fetchMeeting(route.params.id as string);
+      // start date epoch at ytoday 0 hour
+      const startDate = new Date();
+      startDate.setHours(0, 0, 0, 0);
+      // get number of 15 minute intervals
+      const start15MinBlock = startDate.getTime() / (1000 * 60 * 15);
+
+      const endDate = new Date(meeting.value.by_end_date)
+      const end15MinBlock = endDate.getTime() / (1000 * 60 * 15);
+
+
       meeting.value.availability = [
         {
           name: "Jack",
-          slots: [1, 2, 3,4 ,5],
+          slots: generateData(100, { min: start15MinBlock, max: end15MinBlock }),
         },
+        {
+          name: "Jill",
+          slots: generateData(100, { min: start15MinBlock, max: end15MinBlock }),
+        },
+        {
+          name: "Jim",
+          slots: generateData(100, { min: start15MinBlock, max: end15MinBlock }),
+        },
+        {
+          name: "Jen",
+          slots: generateData(100, { min: start15MinBlock, max: end15MinBlock }),
+        }
       ]
     };
 
