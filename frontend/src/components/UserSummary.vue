9cd67cce2769e7387ec50dacbd126646513ff4ba
Jack Driscoll <jdriscoll98@ufl.edu>
2021-10-02 11:46:27 -0400

    feature #3 overlap %

--- frontend/src/components/UserSummary.vue
+++ frontend/src/components/UserSummary.vue
@@ -3,8 +3,8 @@
   <i>See your availability stats and make adjustments</i>
   <div class="stats">
     <div>Total availability: {{ totalAvailability }}</div>
-    <div>Longest you can meet: <span class="number">30</span> min</div>
-    <div>Ovelap with others: <span class="number">72</span> %</div>
+    <div>Longest you can meet: {{ longestMeeting }}</div>
+    <div>Ovelap with others: {{ overlapWithOthers }}</div>
   </div>
   <Button @click="handleAdjustBtn" label="Adjust my availability" />
 </template>
@@ -47,8 +47,66 @@ export default defineComponent({
       return `${hours} hr ${minutes} min`
     })
 
+    const longestMeeting = computed(() => {
+      const userIndex = meeting.availability.findIndex((a) => {
+        return a.name === activeUser.value
+      })
+      if (userIndex == -1) return 0
+
+      // longest continuous block of time ( using a reducer for fun)
+      let previous
+      let max = 0
+      const longest = meeting.availability[userIndex].slots.reduce(
+        (acc, curr) => {
+          if (acc == 0) {
+            previous = curr
+            return 1
+          } else if (curr - previous == 1) {
+            previous = curr
+            return acc + 1
+          } else {
+            max = acc
+            return max
+          }
+        },
+        0
+      )
+      const longestInMinutes = longest * 15
+      // convert longestInMinutes to hours and minutes
+      const hours = Math.floor(longestInMinutes / 60)
+      const minutes = longestInMinutes % 60
+      return `${hours} hr ${minutes} min`
+    })
+
+    const overlapWithOthers = computed(() => {
+      // count the total number of slots across all users
+      const totalSlots = meeting.availability.reduce((acc, curr) => {
+        return acc + curr.slots.length
+      }, 0)
+      // make a set of all the slots
+      const slots = meeting.availability.reduce((acc, curr) => {
+        return acc.concat(...(curr.slots as any))
+      }, []) as any
+      console.log(slots.length)
+      // total slots that active user has in the set
+      const userIndex = meeting.availability.findIndex((a) => {
+        return a.name === activeUser.value
+      })
+      if (userIndex == -1) return 0
+      const userSlots = Array.from(meeting.availability[userIndex].slots)
+      const overlap = slots.filter((slot) => {
+        return userSlots.includes(slot)
+      })
+      console.log(overlap.length)
+      // return perecentage of overlap to slots roudned to 2 decimal places
+      const fraction = (overlap.length / totalSlots) * 100
+      return `${fraction.toFixed(2)}%`
+    })
+
     return {
       totalAvailability,
+      longestMeeting,
+      overlapWithOthers,
       handleAdjustBtn
     }
   }
