9fca38de872ed440c43ef395a8debe4eb978b54e
Jack Driscoll <jdriscoll98@ufl.edu>
2021-10-02 13:11:57 -0400

    name input styles

--- frontend/src/components/NameInput.vue
+++ frontend/src/components/NameInput.vue
@@ -1,30 +1,45 @@
 <template>
-  <div id="name-group" class="p-field p-text-left p-d-flex p-flex-column">
-    <label class="p-text-bold" for="name">What is this meeting for?</label>
-    <InputText
-      id="name"
-      type="text"
-      v-model="title"
-      placeholder="The Grandiose Meeting"
-      :class="{ 'p-invalid': error }"
-      class="p-shadow-5 p-inputtext-lg"
-    />
+  <div id="name-group">
+    <label class="label p-mb-2" for="name">What's its name?</label>
+    <em class="assitive">Type the meeting name you want here</em>
+    <input class="input p-mt-2" v-model="title" />
+
     <p v-if="error" id="name-help" class="p-error p-text-bold p-mt-2">
       <i class="pi pi-exclamation-triangle p-mr-1"></i>{{ error }}
     </p>
   </div>
 </template>
 
-<script lang="ts">
-import { ref, toRefs, watch } from 'vue'
+<style>
+.label {
+  font-size: 1.5rem;
+  font-weight: bold;
+  display: block;
+}
 
-import InputText from 'primevue/inputtext'
+.assitive {
+  font-size: 1.5rem;
+  opacity: 0.5;
+}
+
+.input {
+  height: 2.5rem;
+  width: 100%;
+  background: none;
+  color: var(--text);
+  font-size: 1.5rem;
+  outline: none;
+  border: none;
+  border-bottom: 2px solid var(--secondary);
+}
+</style>
+
+<script lang="ts">
+import { ref, watch } from 'vue'
 
 export default {
   name: 'NameInput',
-  components: {
-    InputText
-  },
+  components: {},
   emits: ['update:title'],
   props: {
     error: {
