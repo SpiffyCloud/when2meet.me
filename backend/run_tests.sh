81a30cab431469321725a650c291a67f3cd0f3be
Dayron Edmundo Acosta <dayroned@gmail.com>
2021-10-08 03:17:39 +0000

    disable backend tests workflow because of bug

--- backend/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# docker exec django-app python manage.py test
-# docker exec django-app coverage run --source='./api' manage.py test
-# docker exec django-app coverage report
-# docker exec django-app coverage html
-
-echo "OK"
