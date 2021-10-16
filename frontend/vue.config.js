7ffe4f6793d4d939e48f5aae90e2805790f1a02c
Alex Payne <alexpayne@MacBook-Pro.local>
2021-10-16 16:50:22 -0400

    added page title

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,5 +6,13 @@ module.exports = {
     // Debugging config
     configureWebpack: {
         devtool: 'source-map'
+    },
+    pages: {
+      index: {
+        entry: 'src/main.ts',
+        template: 'public/index.html',
+        filename: 'index.html',
+        title: 'When 2 Meet Me'
+      }
     }
 }
